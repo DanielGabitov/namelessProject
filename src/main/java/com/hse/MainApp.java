0ad98826b8f8a685c9c65dc5c12ca40752ff2f5e
Даниил Габитов <work.gabitov@gmail.com>
2021-05-10 14:22:51 +0300

    clean main

--- src/main/java/com/hse/MainApp.java
+++ src/main/java/com/hse/MainApp.java
@@ -12,11 +12,6 @@ import java.util.Base64;
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) throws IOException {
-        File file   = new File("C:\\Users\\ASUS\\Desktop\\HELP.png");
-        byte[] data = FileUtils.readFileToByteArray(file);
-        String encodedString = Base64.getEncoder().encodeToString(data);
-        File newFile   = new File("C:\\Users\\ASUS\\Desktop\\test.png");
-        FileUtils.writeByteArrayToFile(newFile, Base64.getDecoder().decode(encodedString));
-//        new SpringApplication(MainApp.class).run(args);
+        new SpringApplication(MainApp.class).run(args);
     }
 }
