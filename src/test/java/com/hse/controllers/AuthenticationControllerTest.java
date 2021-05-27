5ff1e256c7813d0618b9c6f06ec9f04bf5738739
egiptipavel <kinqstone585@gmail.com>
2021-05-27 21:13:37 +0300

    Add some tests

---
+++ src/test/java/com/hse/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,66 @@
+package com.hse.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestPropertySource("/application-test.properties")
+@Sql(value = {"/scripts/before-test.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class AuthenticationControllerTest {
+    private final MockMvc mockMvc;
+
+    @Autowired
+    public AuthenticationControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    @Sql(value = {"/scripts/before-auth-test.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void testAuthentication() throws Exception {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("username", List.of("username"));
+        map.put("password", List.of("password"));
+
+        mockMvc.perform(
+                post("/api/authentication")
+                        .params(map)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .containsHeader(HttpHeaders.AUTHORIZATION);
+    }
+
+    @Test
+    public void testFailAuthentication() throws Exception {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("username", List.of("username"));
+        map.put("password", List.of("password"));
+
+        mockMvc.perform(
+                post("/api/authentication")
+                        .params(map)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(header().doesNotExist(HttpHeaders.AUTHORIZATION));
+    }
+}
---
+++ src/test/resources/scripts/before-test.sql
@@ -0,0 +1,7 @@
+DELETE FROM postgres.public.events;
+DELETE FROM postgres.public.events_images;
+DELETE FROM postgres.public.events_participants;
+DELETE FROM postgres.public.events_organizers;
+DELETE FROM postgres.public.users;
+DELETE FROM postgres.public.users_images;
+DELETE FROM postgres.public.likes;
